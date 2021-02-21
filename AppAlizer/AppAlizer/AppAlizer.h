dcb1ccbd0f9a6355170795bebc02b35a80fb483f
fulgido <>
2021-02-21 20:11:01 +0100

    Changed folder and type project

---
+++ AppAlizer/AppAlizer/AppAlizer.h
@@ -0,0 +1,18 @@
+//
+//  AppAlizer.h
+//  AppAlizer
+//
+//  Created by Sofia Fulgido on 21/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppAlizer.
+FOUNDATION_EXPORT double AppAlizerVersionNumber;
+
+//! Project version string for AppAlizer.
+FOUNDATION_EXPORT const unsigned char AppAlizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppAlizer/PublicHeader.h>
+
+
